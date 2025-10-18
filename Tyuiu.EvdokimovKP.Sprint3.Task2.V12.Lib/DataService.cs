f3ea06a41069995f2839d41ba179b6193abf150e
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-19 02:28:04 +0500

    Task2.V12

---
+++ Tyuiu.EvdokimovKP.Sprint3.Task2.V12.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.EvdokimovKP.Sprint3.Task2.V12.Lib
+{
+    public class DataService : ISprint3Task2V12
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double i = 1;
+            do
+            {
+                i = i * (Math.Pow(300 / startValue + Math.Pow(value, startValue), startValue));
+                startValue++;
+            }
+            while (i < stopValue);
+            return i;
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint3.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.EvdokimovKP.Sprint3.Task2.V12.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint3.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 5;
+            int value = 5;
+            double res = ds.GetMultiplySeries(startValue, stopValue, value);
+            double wait = 305;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
