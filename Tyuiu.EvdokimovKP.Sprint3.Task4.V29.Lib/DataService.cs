ba8a22b2308bdcd56808969b99d3eb42747476b3
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-25 20:15:14 +0500

    Task4.V29

---
+++ Tyuiu.EvdokimovKP.Sprint3.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.EvdokimovKP.Sprint3.Task4.V29.Lib
+{
+    public class DataService : ISprint3Task4V29
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+                if (x == 0)
+                    break;
+                else
+                    res = res + Math.Cos(x) / x;
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint3.Task4.V29/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.EvdokimovKP.Sprint3.Task4.V29.Lib;
+
+DataService ds  = new DataService();
+
+Console.WriteLine("********************************************************************");
+Console.WriteLine("Исходные данные:                                                    ");
+Console.WriteLine("********************************************************************");
+
+int startValue = -5;
+Console.WriteLine("Начало" + startValue);
+int stopValue = 5;
+Console.WriteLine("Начало" + stopValue);
+
+Console.WriteLine(ds.Calculate(startValue, stopValue));
